edf1bd51e27185b9ebce6422b7aad091fe31a734
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-19 08:41:08 +1000

    copy from root

---
+++ src/main/java/org/codemucker/jmutate/generate/CodeGenMeta.java
@@ -0,0 +1,6 @@
+    package org.codemucker.jmutate.generate;
+
+    public class CodeGenMeta {
+
+        public static final String matcherGenerator = "project=JMutate;dependency=org.codemucker:codemucker-jmutate:0.1;build=2015-04-28T09:45:34Z;generator=org.codemucker.jmutate.generate.matcher.MatcherGenerator";
+    }
